cb0ab670ab6ac27809cd960b10a731ac7a09bd88
Ant2235 <ortizant.2009@gmail.com>
2025-11-17 21:01:56 +0000

    start on js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 console.log("script started");
 
+function addTask() {
+    let textBox = document.getElementById("task-input");
+    let taskText = textBox.value;
+    textBox.value = "";
+
+    let taskList = document.getElementById("task-list");
+
+}
