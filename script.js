5fd9f763df9d7c3a6af538914cb71d1cc69d4fe0
Ant2235 <ortizant.2009@gmail.com>
2025-11-18 21:03:46 +0000

    some more js

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 console.log("script started");
 
 function addTask() {
-    let textBox = document.getElementById("task-input");
+    let textBox = document.getElementById("input");
+    //console.log(textBox)
     let taskText = textBox.value;
     textBox.value = "";
 
     let taskList = document.getElementById("task-list");
 
+    let taskDiv = document.createElement("div");
+    taskDiv.id = "task" + taskList.childElementCount;
+    console.log(taskDiv)
+    taskDiv.classList.add("task-item");
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = "checkbox" + taskList.childElementCount;
+
+    let lable = document.createElement("label");
+    lable.id = "lable " + taskList.childElementCount;
+    lable.innerText = taskText;
+
+    taskDiv.appendChild(checkbox);
+    taskDiv.appendChild(lable);
+    taskList.appendChild(taskDiv);
 }
