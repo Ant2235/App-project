e286b1761d52b2115be8da97689a79ebc3dd57ad
Ant2235 <ortizant.2009@gmail.com>
2025-11-19 21:04:41 +0000

    add task works now working on removing task

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ function addTask() {
     //console.log(textBox)
     let taskText = textBox.value;
     textBox.value = "";
+    if (taskText == ""){
+        alert("Please enter a task");
+        return
+    }
 
-    let taskList = document.getElementById("task-list");
+    let taskList = document.getElementById("main");
 
     let taskDiv = document.createElement("div");
     taskDiv.id = "task" + taskList.childElementCount;
-    console.log(taskDiv)
+    //console.log(taskDiv)
     taskDiv.classList.add("task-item");
 
     let checkbox = document.createElement("input");
@@ -24,4 +28,34 @@ function addTask() {
     taskDiv.appendChild(checkbox);
     taskDiv.appendChild(lable);
     taskList.appendChild(taskDiv);
+}
+
+function generateIdNum() {
+    // Start with idNum = 0
+    let idNum = 0;
+
+    // Check if a task with that id exist
+    while (localStorage.getItem("task" + idNum) != null) {
+        idNum++;
+    }
+    // If it doesn, increment idNum and check again
+    return idNum;
+}
+
+
+function removeTask(event) {
+    let checkboxId = event.target.id;
+    let idNum = checkboxId.substring(8);
+    let taskDiv = document.getElementById("task" + idNum);
+    taskDiv.classList.add("remove");
+
+    let tasklist = document.getElementById("main");
+
+    setTimeout(function () {
+        tasklist.removeChild(taskDiv);
+        fixTaskColors();
+        localStorage.removeItem(taskDiv.id);
+    }, 1000)
+    
+
 }
